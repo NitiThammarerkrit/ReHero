9dab71d9841ee8140eae1cb7f23ebde51ade8b64
Niti Thammarerkrit <smallnick007@hotmail.com>
2018-11-23 18:35:59 +0700

    Monster Skill System (pls change "monsterSprite.png" to "monsterSprite")

--- ReHero/Hero.h
+++ ReHero/Hero.h
@@ -8,6 +8,8 @@ class Hero : public SpriteObject
 {
 	int maxHP;
 	int HP;
+	int defArmor;
+	bool isPoisoned;
 
 public:
 	Hero(int HP, string fileName, int row, int column);
@@ -16,10 +18,16 @@ public:
 	virtual void update(float deltaTime);
 	int getHP();
 	int getMaxHP();
+	int getArmor();
 	void setHP(int amount);
 	void setMaxHP(int amount);
 	bool takeDamage(int damage); //true if alive, false if die
-	void heal(int amount);
+	void getHeal(int amount);
+	void gainArmor(int amount);
+	void takePoison();
+
+	void startTurn(); //resolve every special condition & remove armor from last turn
+
 	int state;
 	int c;
 
--- ReHero/Monster.cpp
+++ ReHero/Monster.cpp
@@ -3,17 +3,30 @@
 #include "Monster.h"
 #include "Game.h"
 
+#define POISON_DMG 2
+
 using namespace std;
 
-Monster::Monster(int HP, string fileName, int row, int column) : SpriteObject(fileName, row, column) {
+Monster::Monster(int HP, string name, int row, int column) : SpriteObject(name + ".png", row, column) {
+	srand(time(NULL));
+	
 	this->maxHP = HP;
 	this->HP = HP;
+	this->name = name;
+	defArmor = 0;
+	isPoisoned = false;
+
 	state = 0;
 	c = 0;
+
+	skillData = new ifstream(name + ".txt");
+
+	randomUseSkill(nullptr, nullptr);
+
 }
 
 Monster::~Monster() {
-
+	skillData->close();
 }
 
 void Monster::update(float deltaTime)
@@ -34,22 +47,22 @@ void Monster::update(float deltaTime)
 			state = 1;
 		}
 		else
-		if (state == 1)
-		{
-			c += deltaTime;
-			if (c > 500)
+			if (state == 1)
 			{
-				state = 2;
-				c = 0;
+				c += deltaTime;
+				if (c > 500)
+				{
+					state = 2;
+					c = 0;
+				}
 			}
-		}
-		else
-		if (state == 2)
-		{
-			this->translate(glm::vec3(550, 0, 0));
-			state = 0;
-			Game::getInstance()->state = 3;
-		}
+			else
+				if (state = 2)
+				{
+					this->translate(glm::vec3(550, 0, 0));
+					state = 0;
+					Game::getInstance()->state = 3;
+				}
 	}
 }
 
@@ -59,6 +72,10 @@ int Monster::getHP() {
 
 int Monster::getMaxHP() {
 	return this->maxHP;
+}
+
+int Monster::getArmor() {
+	return this->defArmor;
 }
 
 void Monster::setHP(int amount) {
@@ -84,7 +101,8 @@ bool Monster::takeDamage(int damage) {
 	}
 }
 
-void Monster::heal(int amount) {
+void Monster::getHeal(int amount) {
+	cout << name << " heal " << amount << " to himself" << endl;
 	if (this->HP + amount > this->maxHP)
 	{
 		this->HP = this->maxHP;
@@ -92,5 +110,100 @@ void Monster::heal(int amount) {
 	else
 	{
 		this->HP += amount;
+	}
+	
+}
+
+void Monster::gainArmor(int amount) {
+	defArmor += amount;
+	cout << name << "gain " << amount << " armor" << endl;
+}
+
+void Monster::takePoison() {
+	isPoisoned = true;
+}
+
+void Monster::startTurn() {
+	defArmor = 0;
+	if (isPoisoned)
+	{
+		cout << name << " take " << POISON_DMG << " damage from Poison."<< endl;
+		HP -= POISON_DMG;
+	}
+}
+
+void Monster::randomUseSkill(Hero * enemyTarget, Monster * friendTarget) {
+	if (skillData == nullptr)
+	{
+		cout << "data file lost" << endl;
+		return;
+	}
+	
+	//this monster has how many skill?
+	int howManySkill;
+	*skillData >> howManySkill;
+
+	//random select one
+	int selectedSkill = rand() % howManySkill;
+	string skill;
+	getline(*skillData, skill, '\n');
+
+	//loop for every skills
+	for (int i = 0; i < howManySkill; i++)
+	{
+		getline(*skillData, skill, '\n');
+		if (i == selectedSkill)
+		{
+			//get selected skill's name first
+			stringstream usedSkill(skill);
+			string effect;
+			int value;
+			getline(usedSkill, effect, '\t');
+			cout << name << " use " << effect << endl;
+			while (1)
+			{
+				//read data of the selected skill (each action)
+				usedSkill >> effect >> value;
+
+				//perform a skill
+				if (effect == "damage") doDamage(enemyTarget, value);
+				else if (effect == "heal") heal(friendTarget, value);
+				else if (effect == "poison") usePoison(enemyTarget);
+				else if (effect == "defend") gainArmor(value);
+				else break;
+			}
+
+			break;
+		}
+
+	}
+}
+
+void Monster::doDamage(Hero * target, int damage) {
+	if (target != nullptr)
+	{
+		target->takeDamage(damage);
+		cout << name << " deal " << damage << " damage to Hero" << endl;
+	}
+}
+
+void Monster::heal(Monster * target, int amount) {
+	if (target != nullptr)
+	{
+		target->getHeal(amount);
+		cout << "heal " << amount << " for " << target->name << endl;
+	}
+	else
+	{
+		this->getHeal(amount);
+	}
+	
+}
+
+void Monster::usePoison(Hero * target) {
+	if (target != nullptr)
+	{
+		target->takePoison();
+		cout << name << " use poison to Hero" << endl;
 	}
 }
